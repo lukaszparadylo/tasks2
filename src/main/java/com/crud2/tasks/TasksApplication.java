e8da0d093bace2472ae60399d81cbb0be21685c7
lukaszparadylo <lukaszparadylo@gmail.com>
2021-09-29 19:28:22 +0200

    Add comment to main file

--- src/main/java/com/crud2/tasks/TasksApplication.java
+++ src/main/java/com/crud2/tasks/TasksApplication.java
@@ -11,8 +11,8 @@ public class TasksApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(TasksApplication.class, args);
 	}
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(TasksApplication.class);
-	}
+	//@Override
+	//protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	//	return application.sources(TasksApplication.class);
+	//}
 }

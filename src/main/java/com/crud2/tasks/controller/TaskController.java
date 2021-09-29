5cb7eb648bb0e756ff8b9c5d235e9af6469133bb
lukaszparadylo <lukaszparadylo@gmail.com>
2021-09-29 18:55:34 +0200

    First attempt to Heroku

--- src/main/java/com/crud2/tasks/controller/TaskController.java
+++ src/main/java/com/crud2/tasks/controller/TaskController.java
@@ -11,6 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/v1/task")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class TaskController {
 
     private final DbService service;

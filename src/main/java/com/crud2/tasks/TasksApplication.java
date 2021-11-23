c280e7ab619866d634747d583f3160d02ec8706f
lukaszparadylo <lukaszparadylo@gmail.com>
2021-11-23 19:15:40 +0100

    next version of the program 4

--- src/main/java/com/crud2/tasks/TasksApplication.java
+++ src/main/java/com/crud2/tasks/TasksApplication.java
@@ -18,15 +18,14 @@ import javax.mail.MessagingException;
 import java.io.IOException;
 
 @SpringBootApplication
-public class TasksApplication {
+public class TasksApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(TasksApplication.class, args);
 	}
 
-
-	//@Override
-	//protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-	//	return application.sources(TasksApplication.class);
-	//}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TasksApplication.class);
+	}
 }
